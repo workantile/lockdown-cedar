16d7ebce471913790eb8a286ec2455e5cf46ab71
Tom Brandt <tom.brandt@northtech.com>
2012-12-31 10:41:32 -0500

    configure time zone correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :set_time_zone
-
   def after_sign_in_path_for(resource)
   	admin_admins_path
   end
-  
-  def set_time_zone
-  	Time.zone = "America/New_York"
-  end
+
 end
