81d69b79ed6c0f32b4597bac14e90e7f99eaf3dd
Tom Brandt <tom.brandt@northtech.com>
2013-03-09 13:21:01 -0500

    add mainatinable success/error responses to door controllers

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -3,19 +3,21 @@ class AccessController < ApplicationController
 		@door_controller = DoorController.find_by_address(params[:address])
 		if @door_controller
 			grant_access
-			render :content_type => "text/plain", :layout => false
 		else
 			render :text => 'ERROR'
 		end
 	end
 
 	def grant_access
-		if Member.grant_access?(params[:rfid], params[:address])
+		@member = Member.find_by_rfid(params[:rfid])
+    @member ||= Member.find_by_rfid(params[:rfid].downcase)
+		if @member.access_enabled?
 			@response = @door_controller.success_response
 			send_email
 		else
 			@response = @door_controller.error_response
 		end
+		render :content_type => "text/plain", :layout => false
 	end
 
 	def send_email
--- spec/controllers/access_controller_spec.rb
+++ spec/controllers/access_controller_spec.rb
@@ -2,16 +2,34 @@ require 'spec_helper'
 
 describe AccessController do
 	before(:each) do
-		@member = FactoryGirl.create(:full_member)
-		@door_controller = FactoryGirl.create(:door_controller)
-		get :show, :address => @door_controller.address, :rfid => @member.rfid
+		@member = stub_model(Member, :member_type => 'current', :billing_plan => 'full', :rfid => '1234')
+		Member.stub(:find_by_rfid) { @member }
+		@door_controller = stub_model(DoorController, :address => "abc", :success_response => "OK")
+		DoorController.stub(:find_by_address) { @door_controller }
 	end
 
 	it "assigns to @door_controller" do
+		get :show, :address => @door_controller.address, :rfid => @member.rfid
 		assigns(:door_controller).should eq(@door_controller)
 	end
 
+	it "assigns to @member" do
+		get :show, :address => @door_controller.address, :rfid => @member.rfid
+		assigns(:member).should eq(@member)
+	end
+
+	it "sees if the member should be granted access" do
+		@member.should_receive(:access_enabled?)
+		get :show, :address => @door_controller.address, :rfid => @member.rfid
+	end
+
+	it "sees if the member should be sent an email" do
+		@member.should_receive(:send_usage_email)
+		get :show, :address => @door_controller.address, :rfid => @member.rfid
+	end	
+
 	it "renders the show template" do
+		get :show, :address => @door_controller.address, :rfid => @member.rfid
 		response.should render_template('show')
 	end
 
