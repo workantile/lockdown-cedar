fa0b9e2d36993270f8ea71af4773c6224c776177
twbrandt <twbrandt@gmail.com>
2014-01-07 15:19:52 -0500

    aaa

--- app/models/all_member_event.rb
+++ app/models/all_member_event.rb
@@ -22,6 +22,7 @@ class AllMemberEvent < ActiveRecord::Base
   end
 
   def formatted_display
+    puts scheduled.inspect
     if scheduled.in_time_zone.hour == 0
       scheduled.strftime("%m/%d/%Y all day")
     else
